de213ba01b0a4f67e0856803dad3fb37e9cf80f2
mpetrole <mattpetrole@gmail.com>
2017-02-08 16:54:02 -0500

    Added ETH support

--- jact.rb
+++ jact.rb
@@ -1,13 +1,15 @@
 require 'coinbase/wallet'
 
-client = Coinbase::Wallet::Client.new(api_key:"", api_secret:"")
-
+puts "JAC-T, Just Another Coinbase Ticker"
+bitprice = Hash.new  #the api pulls in hashes, so I need to make hashes
+ethhash = Hash.new
+client = Coinbase::Wallet::Client.new(api_key:"", api_secret:"")  #sets up a fake wallet to get values from the api
 loop do
-  print "Updating...   \r"
-pricehash = client.spot_price({currency_pair: 'BTC-USD'})
-price = Hash.new
-price = pricehash
-
-print "#{price.amount.to_f}    \r"
+  print "Updating...                                                \r"
+bithash = client.spot_price({currency_pair: 'BTC-USD'})  #this gets the bitcoin price converted to USD
+bitprice = bithash
+ethhash = client.exchange_rates({currency: 'ETH'})   #for some reason, the api doesn't support eth-usd, so I had to get janky. Basically, I pull in an array of hashes for eth prices and then get the currency that I want (USD)
+ethprice = ethhash.rates
+print "Bitcoin: #{bitprice.amount.to_f} Etherium: #{ethprice.USD.to_f}   \r"
 sleep 60
 end
